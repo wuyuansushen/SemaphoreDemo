1e022b2a8fbb4938338fb940bf5b6545f8438f4b
wuyuansushen <wuyuansushen@gmail.com>
2021-11-06 20:40:22 +0800

    Sync SemaphoreSlim and IndexI state

--- SemaphoreDemo/Program.cs
+++ SemaphoreDemo/Program.cs
@@ -3,41 +3,49 @@ using System.Threading;
 
 namespace SemaphoreDemo
 {
+    public class SemaphoreIndex
+    {
+        public SemaphoreSlim _pool;
+        public int indexI;
+    }
     class Program
     {
-        private static SemaphoreSlim _pool;
+        //private static SemaphoreSlim _pool;
 
-        //Default value is 0
-        private static int _padding=0;
         static void Main(string[] args)
         {
+            var syncI = new SemaphoreIndex();
+            syncI.indexI = 0;
             //Current Semaphore number means how many vacanies are waiting for used here. 
-            _pool = new SemaphoreSlim(0,3);
+            syncI._pool = new SemaphoreSlim(1,3);
+            Console.WriteLine($"--------------------\n| Single Thread |\n--------------------");
             for(int i=0;i<5;i++)
             {
                 Thread t = new Thread(ThreadProc);
-                t.Start(i);
+                syncI.indexI = i;
+                t.Start(syncI);
             }
             Thread.Sleep(3000);
-
-            Console.WriteLine($"Semaphore call Release(3), make other threads use it.");
-            _pool.Release(3);
+            syncI._pool.Release(2);
+            Console.WriteLine($"--------------------\n| Three thread parallel |\n--------------------");
             Console.WriteLine($"Main thread exits.\n");
         }
 
-        private static void ThreadProc(object num)
+        private static void ThreadProc(object state)
         {
+            var num = ((SemaphoreIndex)state).indexI;
+            var _pool = ((SemaphoreIndex)state)._pool;
             Console.WriteLine($"Thread {(int)num} starts.\nThread {(int)num} waits for the semaphore.\n");
             _pool.Wait();
-            int padding = Interlocked.Add(ref _padding, 500);
-            Console.WriteLine($"Thread {(int)num} Enter the semaphore");
+            _pool.Wait();
+            Console.WriteLine($"Thread {(int)num} Enter the semaphore.");
 
-            Thread.Sleep(1500 + padding);
+            Thread.Sleep(1500);
 
             Console.WriteLine($"Thread {(int)num} releases the semephore.");
 
             //Release and show count previously.
-            Console.WriteLine($"Thread {(int)num} previous semaphore count: {_pool.Release(1)}\nPresent semaphore count: {_pool.CurrentCount}\n");
+            Console.WriteLine($"Thread {(int)num} previous semaphore count: {_pool.Release(2)}\nPresent semaphore count: {_pool.CurrentCount}\n");
         }
     }
 }
