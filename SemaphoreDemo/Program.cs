1735f8ececa081fac8ea721543152365e742368c
wuyuansushen <wuyuansushen@gmail.com>
2021-11-06 21:25:22 +0800

    Remove static global lock usage. Transfer lock for synchronization directly.

--- SemaphoreDemo/Program.cs
+++ SemaphoreDemo/Program.cs
@@ -6,6 +6,7 @@ namespace SemaphoreDemo
     public class SemaphoreIndex
     {
         public SemaphoreSlim _pool;
+        public EventWaitHandle indexTrigger;
         public int indexI;
     }
     class Program
@@ -15,16 +16,18 @@ namespace SemaphoreDemo
         static void Main(string[] args)
         {
             var syncI = new SemaphoreIndex();
+            syncI.indexTrigger = new EventWaitHandle(false, EventResetMode.AutoReset);
             syncI.indexI = 0;
             //Current Semaphore number means how many vacanies are waiting for used here. 
             syncI._pool = new SemaphoreSlim(1,3);
             Console.WriteLine($"--------------------\n| Single Thread |\n--------------------");
-            for(int i=0;i<5;i++)
+            while (syncI.indexI < 5)
             {
+                syncI.indexTrigger.Reset();
                 Thread t = new Thread(ThreadProc);
-                syncI.indexI = i;
                 t.Start(syncI);
-                Thread.Sleep(500);
+                syncI.indexTrigger.WaitOne();
+                Interlocked.Increment(ref syncI.indexI);
             }
             Thread.Sleep(3000);
             syncI._pool.Release(2);
@@ -34,10 +37,10 @@ namespace SemaphoreDemo
 
         private static void ThreadProc(object state)
         {
+            ((SemaphoreIndex)state).indexTrigger.Set();
             var num = ((SemaphoreIndex)state).indexI;
             var _pool = ((SemaphoreIndex)state)._pool;
             Console.WriteLine($"Thread {(int)num} starts.\nThread {(int)num} waits for the semaphore.\n");
-            _pool.Wait();
             _pool.Wait();
             Console.WriteLine($"Thread {(int)num} Enter the semaphore.");
 
@@ -46,7 +49,7 @@ namespace SemaphoreDemo
             Console.WriteLine($"Thread {(int)num} releases the semephore.");
 
             //Release and show count previously.
-            Console.WriteLine($"Thread {(int)num} previous semaphore count: {_pool.Release(2)}\nPresent semaphore count: {_pool.CurrentCount}\n");
+            Console.WriteLine($"Thread {(int)num} previous semaphore count: {_pool.Release(1)}\nPresent semaphore count: {_pool.CurrentCount}\n");
         }
     }
 }
